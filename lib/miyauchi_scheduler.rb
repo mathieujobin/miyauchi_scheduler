78905376be1a6d73bef04dbb58c74f3e4be688b8
Mathieu Jobin <mathieu@justbudget.com>
2015-06-23 22:48:03 +0900

    beginning of implementation

--- lib/miyauchi_scheduler.rb
+++ lib/miyauchi_scheduler.rb
@@ -1,13 +1,86 @@
 require "miyauchi_scheduler/version"
 require "miyauchi_calendar"
 
+DaysPerMonth = {
+  1 => 31,
+  2 => 29, # ouch, fix this
+  3 => 31,
+  4 => 30,
+  5 => 31,
+  6 => 30,
+  7 => 31,
+  8 => 31,
+  9 => 30,
+  10 => 31,
+  11 => 30,
+  12 => 31
+}
 
 class MiyauchiScheduler
+
   def initialize(params={})
+    # setting defaults for Otousan
+    params[:worker_per_day]     ||= 2
+    params[:available_workers]  ||= 4
+    params[:days_off_per_month] ||= 8
+    params[:days_off_per_year]  ||= 19
+    params[:current_month]      ||= current_month
+    @params = params
+    @working_schedule = MiyauchiCalendar.new(days)
+    @days_off = MiyauchiCalendar.new(days)
   end
 
   def generate_calendar
-    MiyauchiCalendar.new
+    days.times do |d|
+      while @working_schedule.worker_on(d+1).size < worker_per_day
+        w = random_worker
+        #unless @working_schedule.worker_on(d+1).include? w
+          @working_schedule.add_worker(w, d+1)
+        #end
+      end
+    end
+    @working_schedule
   end
 
+private
+
+  attr_reader :params
+
+  def days
+    DaysPerMonth[current_month]
+  end
+
+  def workers
+    @workers ||= available_workers.times.map { |x| "worker #{x+1}" }
+  end
+
+  def random_worker
+    workers[(rand * available_workers).to_i]
+  end
+
+  def worker_per_day
+    params[:worker_per_day]
+  end
+
+  def available_workers
+    params[:available_workers]
+  end
+
+  def days_off_per_month
+    params[:days_off_per_month]
+  end
+
+  def days_off_per_year
+    params[:days_off_per_year]
+  end
+
+  def current_month
+    Time.now.strftime('%m').to_i
+  end
+
+  def next_month
+    r = current_month + 1
+    r > 12 ? 1 : r
+  end
 end
+
--- spec/miyauchi_scheduler_spec.rb
+++ spec/miyauchi_scheduler_spec.rb
@@ -11,8 +11,9 @@ describe MiyauchiScheduler do
 
   it 'each days should have two workers by default' do
     cal = subject.generate_calendar
-    cal.days.each do |workers|
+    cal.days.each do |day, workers|
       expect(workers.size).to eq(2)
     end
   end
 end
+
